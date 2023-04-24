fef4c4109127ee310e0a0273c948acb21639c284
Dmitry Jum <xfiler@ymail.com>
2023-04-23 17:09:59 -0700

    creates a new db migration to rename the table

--- prisma/migrations/20230424000329_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Error" (
-    "id" SERIAL NOT NULL,
-    "message" VARCHAR(255) NOT NULL,
-    "error_details" JSONB,
-    "application_id" TEXT,
-
-    CONSTRAINT "Error_pkey" PRIMARY KEY ("id")
-);
